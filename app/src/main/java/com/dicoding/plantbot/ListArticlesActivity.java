0b8d0df3d36e23f6cd951f39a9342b0042e9d35b
Fauziah Reza Oktaviyani <fzhokta@gmail.com>
2021-01-02 16:39:12 +0700

    baru5

---
+++ app/src/main/java/com/dicoding/plantbot/ListArticlesActivity.java
@@ -0,0 +1,59 @@
+package com.dicoding.plantbot;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.dicoding.plantbot.Adapter.ArticleAdapter;
+import com.dicoding.plantbot.Model.ArticleList;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListArticlesActivity extends AppCompatActivity {
+    RecyclerView rv;
+    List<ArticleList>articleLists;
+    DatabaseReference databaseReference;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_articles);
+        rv=findViewById(R.id.rec);
+        rv.setHasFixedSize(true);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        DividerItemDecoration decoration=new DividerItemDecoration(this,DividerItemDecoration.VERTICAL);
+        rv.addItemDecoration(decoration);
+        articleLists=new ArrayList<>();
+        databaseReference= FirebaseDatabase.getInstance().getReference("Articles");
+        getImageData();
+
+    }
+
+    private void getImageData() {
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot di:dataSnapshot.getChildren()){
+                    ArticleList articleList=di.getValue(ArticleList.class);
+                    articleLists.add(articleList);
+                }
+                ArticleAdapter adapter=new ArticleAdapter(articleLists,getApplicationContext());
+                rv.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
