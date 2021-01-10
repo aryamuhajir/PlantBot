b20f37a41473638d9e08f61c90cf2e94515c801d
aryamuhajir <aryamuhajir888@gmail.com>
2021-01-11 02:52:43 +0700

    pindah

--- app/src/main/java/com/dicoding/plantbot/Fragment/ProfileTabFragment.java
+++ app/src/main/java/com/dicoding/plantbot/Fragment/ProfileTabFragment.java
@@ -1,10 +1,14 @@
 package com.dicoding.plantbot.Fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
@@ -13,7 +17,7 @@ import com.dicoding.plantbot.Adapter.ProfileAdapter;
 import com.dicoding.plantbot.R;
 import com.google.android.material.tabs.TabLayout;
 
-public class ProfileTabFragment extends Fragment {
+public class ProfileTabFragment extends Fragment implements View.OnClickListener {
 
 
     @Nullable
@@ -23,4 +27,28 @@ public class ProfileTabFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RelativeLayout btnMoveActivity4 = view.findViewById(R.id.schedule);
+        btnMoveActivity4.setOnClickListener(this);
+
+
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.schedule:
+                Intent intent1 = getContext().getPackageManager().getLaunchIntentForPackage("com.e_carter.userroles");
+                if (intent1 == null) {
+                    intent1 = new Intent(Intent.ACTION_VIEW);
+                    intent1.setData(Uri.parse("market://details?id=" + "com.google.ar.lens"));
+                }
+                intent1.putExtra(Intent.ACTION_SEND , Uri.parse("https://bs.floristic.org/image/o/7210dfc0c2a48bdde194bef37c7b7956974bc1b7"));
+                getContext().startActivity(intent1);
+                break;
+        }
+    }
 }
